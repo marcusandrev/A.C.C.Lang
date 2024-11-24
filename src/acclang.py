f203476d5049c026493699f35b3d7907db8ff2ff
Andre Villanueva <marcusandrev.mav@gmail.com>
2024-11-24 18:02:23 +0800

    feat: initial integration of lexer logic

--- src/acclang.py
+++ src/acclang.py
@@ -1,5 +1,7 @@
 # This file is for the implementation of the GUI
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import os
+from src.packages.lexer.lexer import Lexer
 
 app = Flask(__name__)
 
@@ -9,6 +11,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/lex', methods=['POST'])
+def lex():
+    # file_path = os.path.join(os.path.dirname(__file__), '..', 'Files', 'lexer_test.acc')
+    # with open(file_path, 'r') as file:
+    #     source_code = file.readlines()
+    source_code = request.form['source_code']
+    lexer = Lexer(source_code)
+    lexer.start()
+    token_stream = lexer.token_stream
+    print(token_stream)
+    error_log = lexer.log
+    print(error_log)
+    return jsonify({'token_stream': token_stream, 'error_log': str(error_log)})
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='127.0.0.1', port=5006)
