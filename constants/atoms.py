e6ab9ad225065736a92e7d0aca755b2507397fc8
Austiel <abdielaustero.work@gmail.com>
2024-12-04 16:41:36 +0800

    fix: fixed atoms' ascii to work with new td

--- constants/atoms.py
+++ constants/atoms.py
@@ -1,9 +1,9 @@
 ATOMS = {
     # single characters only
     'ascii': {chr(i) for i in range(128) if chr(i) != '\0'},
-    'ascii_274': {chr(i) for i in range(128) if chr(i) not in ['"', '\\', '\0']},
-    'ascii_283': {chr(i) for i in range(128) if chr(i) not in ['^', '\0']},
-    'ascii_284': {chr(i) for i in range(128) if chr(i) not in ['/', '\0']},
+    'ascii_263': {chr(i) for i in range(128) if chr(i) not in ['"', '\\', '\0']},
+    'ascii_272': {chr(i) for i in range(128) if chr(i) not in ['^', '\0']},
+    'ascii_273': {chr(i) for i in range(128) if chr(i) not in ['/', '\0']},
     'alphabet': {
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
