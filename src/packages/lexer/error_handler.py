cc5b396dd2b2999e3d9b6b591b67e6bb8a8392d8
Austiel <abdielaustero.work@gmail.com>
2024-12-04 17:36:02 +0800

    feat: Added andamhie error handler

--- src/packages/lexer/error_handler.py
+++ src/packages/lexer/error_handler.py
@@ -28,6 +28,19 @@ class DelimError():
         
         return error_message
 
+class UnfinishedAndamhie():
+    def __init__(self, line: str, position: tuple[int, int], delims: list):
+        self._line = line.replace('\n', '')
+        self._position = position
+        self._delims = delims
+
+    def __str__(self):
+        error_message = f"unfinished andamhie literal: expected any {self._delims}\n" \
+                        f" {self._position[0]+1:<5}|{self._line}\n" \
+                        f"      |{' '*self._position[1]}^\n"
+        
+        return error_message
+
 class UnclosedString():
     def __init__(self, line: str, position: tuple[int, int]):
         self._line = line.replace('\n', '')
